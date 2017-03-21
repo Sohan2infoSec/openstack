1fb2095ec44db277042eadbea5a4f46d8f6935d0
Maksim Malchuk <mmalchuk@users.noreply.github.com>
2017-03-22 01:47:58 +0300

    Update adminrc.sh
    to avoid some well-known errors with openstack clients
    it is better not provide the version in the OS_AUTH_URL
    in favor of using OS_IDENTITY_API_VERSION environment variable.

--- openstack-scripts/02-keystone/adminrc.sh
+++ openstack-scripts/02-keystone/adminrc.sh
@@ -4,6 +4,6 @@ export OS_PROJECT_NAME=admin
 export OS_TENANT_NAME=admin
 export OS_USERNAME=admin
 export OS_PASSWORD=Password1
-export OS_AUTH_URL=http://controller:35357/v3
+export OS_AUTH_URL=http://controller:35357/
 export OS_IDENTITY_API_VERSION=3
 export OS_IMAGE_API_VERSION=2
