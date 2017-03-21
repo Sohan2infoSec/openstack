3d92539ae801c5bdb599e041113ea4f6bac437b8
Maksim Malchuk <mmalchuk@users.noreply.github.com>
2017-03-22 01:49:15 +0300

    Update demorc.sh
    to avoid some well-known errors with openstack clients
    it is better not provide the version in the OS_AUTH_URL
    in favor of using OS_IDENTITY_API_VERSION environment variable.

--- openstack-scripts/02-keystone/demorc.sh
+++ openstack-scripts/02-keystone/demorc.sh
@@ -4,6 +4,6 @@ export OS_PROJECT_NAME=demo
 export OS_TENANT_NAME=demo
 export OS_USERNAME=demo
 export OS_PASSWORD=Password1
-export OS_AUTH_URL=http://controller:5000/v3
+export OS_AUTH_URL=http://controller:5000/
 export OS_IDENTITY_API_VERSION=3
 export OS_IMAGE_API_VERSION=2
